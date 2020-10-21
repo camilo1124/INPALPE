b01e8a3a11400853b824a328c6d3f6cde33c55e3
Luis Chavez <lachavezca@unal.edu.co>
2020-10-21 13:34:38 -0500

    version 0.0.7 lista encadena agregada

---
+++ linkedlist.py
@@ -0,0 +1,83 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def deleteNode(head, position): 
+         
+        # Store head node 
+        walker = head 
+ 
+        # If head node itself holds the key to be deleted 
+        if (position == 0): 
+            walker = walker.next
+            return walker
+        
+        for i in range (position-1): 
+            # if key was not present in linked list 
+            if(walker == None): 
+                return head
+            prev = walker 
+            walker = walker.next
+            
+        # desconectando el nodo 
+        walker.next = walker.next.next
+        temp = None
+        return head
+
+#Programa principal
+
+ if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
+
